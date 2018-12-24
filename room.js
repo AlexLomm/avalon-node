53b21044f6215d62eb18701da5441220080d879a
Alexander Lomia <alex.lomia@gmail.com>
2018-12-24 12:12:28 +0400

    ref(ES6 classes): converted the code into ES6 classes

--- room.js
+++ room.js
@@ -1,113 +1,115 @@
-const Room = function (roomId) {
-  this.roomId    = roomId;
-  this.updatedAt = new Date();
-  this.players   = [];
+class Room {
+  constructor(roomId) {
+    this.roomId    = roomId;
+    this.updatedAt = new Date();
+    this.players   = [];
 
-  this.resetState();
-};
-
-Room.prototype.resetState = function () {
-  this.team              = [];
-  this.executionTargetId = null;
-
-  this._lockState();
-
-  return this;
-};
-
-Room.prototype._lockState = function () {
-  this.stateIsLocked = true;
-};
-
-Room.prototype._unlockState = function () {
-  this.stateIsLocked = false;
-};
-
-Room.prototype.toggleTeammate = function (playerId) {
-  this._unlockState();
-
-  const index = this.team.findIndex(id => id === playerId);
-
-  index > -1
-    ? this.team.splice(index, 1)
-    : this.team.push(playerId);
-
-  return this;
-};
-
-Room.prototype.clearTeam = function () {
-  this._unlockState();
-
-  this.team = [];
-
-  return this;
-};
-
-Room.prototype.setExecutionTarget = function (playerId) {
-  this._unlockState();
-
-  this.executionTargetId = playerId;
-
-  return this;
-};
-
-Room.prototype.getState = function () {
-  return {
-    stateIsLocked: this.stateIsLocked,
-    team: this.team,
-    executionTargetId: this.executionTargetId
-  };
-};
-
-Room.prototype.destroy = function () {
-  this.players.forEach(player => this.removePlayer(player.id));
-};
-
-Room.prototype.addPlayer = function (player) {
-  this.removePlayer(player.id);
-
-  player.joinRoom(this.roomId);
-
-  this.players.push(player);
-
-  return this;
-};
-
-Room.prototype.removePlayer = function (playerId) {
-  const index = this.players.findIndex(p => p.id === playerId);
-
-  if (index > -1) {
-    this.players[index].leaveRoom(this.roomId);
-
-    this.players.splice(index, 1);
+    this.resetState();
   }
 
-  return this;
-};
+  resetState() {
+    this.team              = [];
+    this.executionTargetId = null;
 
-Room.prototype.emitToAll = function (eventName, payload) {
-  this.players.forEach((player) => player.emit(eventName, payload));
+    this._lockState();
 
-  return this;
-};
+    return this;
+  }
 
-Room.prototype.emitToAllExcept = function (eventName, payload, playerId) {
-  this.players
+  _lockState() {
+    this.stateIsLocked = true;
+  }
+
+  _unlockState() {
+    this.stateIsLocked = false;
+  }
+
+  toggleTeammate(playerId) {
+    this._unlockState();
+
+    const index = this.team.findIndex(id => id === playerId);
+
+    index > -1
+      ? this.team.splice(index, 1)
+      : this.team.push(playerId);
+
+    return this;
+  }
+
+  clearTeam() {
+    this._unlockState();
+
+    this.team = [];
+
+    return this;
+  }
+
+  setExecutionTarget(playerId) {
+    this._unlockState();
+
+    this.executionTargetId = playerId;
+
+    return this;
+  }
+
+  getState() {
+    return {
+      stateIsLocked: this.stateIsLocked,
+      team: this.team,
+      executionTargetId: this.executionTargetId
+    };
+  }
+
+  destroy() {
+    this.players.forEach(player => this.removePlayer(player.id));
+  }
+
+  addPlayer(player) {
+    this.removePlayer(player.id);
+
+    player.joinRoom(this.roomId);
+
+    this.players.push(player);
+
+    return this;
+  }
+
+  removePlayer(playerId) {
+    const index = this.players.findIndex(p => p.id === playerId);
+
+    if (index > -1) {
+      this.players[index].leaveRoom(this.roomId);
+
+      this.players.splice(index, 1);
+    }
+
+    return this;
+  }
+
+  emitToAll(eventName, payload) {
+    this.players.forEach((player) => player.emit(eventName, payload));
+
+    return this;
+  }
+
+  emitToAllExcept(eventName, payload, playerId) {
+    this.players
       .filter((player) => player.id !== playerId)
       .forEach((player) => player.emit(eventName, payload));
 
-  return this;
-};
+    return this;
+  }
+}
 
 // call touch before each method call
 Object.keys(Room.prototype)
-      .forEach((methodName) => {
-        const method               = Room.prototype[methodName];
-        Room.prototype[methodName] = function (...args) {
-          this.updatedAt = new Date();
+  .forEach((methodName) => {
+    const method               = Room.prototype[methodName];
+    Room.prototype[methodName] = function (...args) {
+      this.updatedAt = new Date();
 
-          return method.apply(this, args);
-        };
-      });
+      return method.apply(this, args);
+    };
+  });
 
 module.exports = Room;
--- rooms-manager.js
+++ rooms-manager.js
@@ -1,49 +1,51 @@
 const Room = require('./room.js');
 
-const RoomsManager = function () {
-  this.rooms = {};
+class RoomsManager {
+  constructor() {
+    this.rooms = {};
 
-  // set up a job for periodically
-  // purging inactive rooms
-  setInterval(() => this.purgeInactiveRooms(), 10 * 60 * 1000);
-};
+    // set up a job for periodically
+    // purging inactive rooms
+    setInterval(() => this.purgeInactiveRooms(), 10 * 60 * 1000);
+  }
 
-RoomsManager.prototype.purgeInactiveRooms = function (inactivityThreshold = 30 * 60 * 1000) {
-  Object.keys(this.rooms).forEach(roomId => {
+  purgeInactiveRooms(inactivityThreshold = 30 * 60 * 1000) {
+    Object.keys(this.rooms).forEach(roomId => {
+      const room = this.get(roomId);
+
+      const inactivityPeriod = new Date() - room.updatedAt;
+
+      if (inactivityPeriod > inactivityThreshold) {
+        room.emitToAll('fetchNodeState', {state: room.resetState().getState()});
+
+        this.destroyRoom(roomId);
+      }
+    });
+  }
+
+  getOrCreate(roomId) {
     const room = this.get(roomId);
 
-    const inactivityPeriod = new Date() - room.updatedAt;
+    if (room) return room;
 
-    if (inactivityPeriod > inactivityThreshold) {
-      room.emitToAll('fetchNodeState', {state: room.resetState().getState()});
+    this.add(roomId);
 
-      this.destroyRoom(roomId);
-    }
-  });
-};
+    return this.get(roomId);
+  }
 
-RoomsManager.prototype.getOrCreate = function (roomId) {
-  const room = this.get(roomId);
+  get(roomId) {
+    return this.rooms[roomId];
+  }
 
-  if (room) return room;
+  add(roomId) {
+    this.rooms[roomId] = new Room(roomId);
+  }
 
-  this.add(roomId);
+  destroyRoom(roomId) {
+    this.rooms[roomId].destroy();
 
-  return this.get(roomId);
-};
-
-RoomsManager.prototype.get = function (roomId) {
-  return this.rooms[roomId];
-};
-
-RoomsManager.prototype.add = function (roomId) {
-  this.rooms[roomId] = new Room(roomId);
-};
-
-RoomsManager.prototype.destroyRoom = function (roomId) {
-  this.rooms[roomId].destroy();
-
-  delete this.rooms[roomId];
-};
+    delete this.rooms[roomId];
+  }
+}
 
 module.exports = RoomsManager;
